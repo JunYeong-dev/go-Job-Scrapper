2d34876151a24e1ed64b5b68334378e087e86916
Jun-cloud <kjy8827@gmail.com>
2020-06-18 21:18:06 +0900

    Check total pages and make URL

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"std/fmt"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -11,10 +12,23 @@ import (
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
-	getPages()
+	totalPages := getPages()
+	fmt.Println(totalPages)
+	for i := 0; i < totalPages; i++ {
+		getPage(i)
+	}
+
 }
 
+// 페이지에 해당하는 URL을 return 하는 함수
+func getPage(page int) {
+	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+	fmt.Println("Requesting", pageURL)
+}
+
+// 전체 페이지를 return 하는 함수
 func getPages() int {
+	pages := 0
 	res, err := http.Get(baseURL)
 	checkErr(err)
 	checkCode(res)
@@ -24,17 +38,21 @@ func getPages() int {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 
-	fmt.Println(doc)
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()
+	})
 
-	return 0
+	return pages
 }
 
+// 페이지를 가져오는데 Error가 발생하는지 체크
 func checkErr(err error) {
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
+// 페이지 코드를 체크
 func checkCode(res *http.Response) {
 	if res.StatusCode != 200 {
 		log.Fatalln("Request failed with Status:", res.StatusCode)
